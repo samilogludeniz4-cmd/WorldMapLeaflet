e7dc88ad046434aedddf9347af6c39f79b3d51e5
Deniz Şamiloğlu <denizsamiloglu@192.168.1.13>
2025-11-11 17:41:43 +0300

    İlk sürüm: Leaflet harita projesi

--- WorldMapLeaflet/obj/Debug/net9.0/WorldMapLeaflet.AssemblyInfo.cs
+++ WorldMapLeaflet/obj/Debug/net9.0/WorldMapLeaflet.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WorldMapLeaflet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a050f5a678f4314f5c97f47079e122d87565852")]
 [assembly: System.Reflection.AssemblyProductAttribute("WorldMapLeaflet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WorldMapLeaflet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
